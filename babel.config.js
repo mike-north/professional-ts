ffd1c8436a2948c178e0028171e245f5e5376166
Mike North <michael.l.north@gmail.com>
2020-10-10 16:42:13 -0700

    updated babel config

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
+    ['@babel/preset-env', { targets: { node: '10' } }],
     '@babel/preset-react',
     '@babel/preset-typescript',
   ],
+  plugins: ['@babel/plugin-proposal-class-properties'],
 };
