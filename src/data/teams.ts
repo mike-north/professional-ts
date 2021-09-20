277c1516a64fcb56331d773ec9d70e47fe480775
timiyay <timothy.asquith@blake.com.au>
2021-09-20 17:12:52 +1000

    add global types

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,16 +1,17 @@
 import { apiCall } from '../utils/networking';
+import type { ITeam } from '../types';
 
-let cachedAllTeamsList: Promise<any[]>;
-export async function getAllTeams() {
+let cachedAllTeamsList: Promise<ITeam[]>;
+export async function getAllTeams(): Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
     cachedAllTeamsList = apiCall('teams');
 
   return await cachedAllTeamsList;
 }
 
-const cachedTeamRecords: Record<string, any> = {};
+const cachedTeamRecords: Record<string, Promise<ITeam>> = {};
 
-export async function getTeamById(id: string) {
+export async function getTeamById(id: string): Promise<ITeam> {
   let cached = cachedTeamRecords[id];
   if (typeof cached === 'undefined')
     cached = cachedTeamRecords[id] = apiCall(`teams/${id}`);
