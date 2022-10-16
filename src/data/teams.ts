ff284995aec4185c604058c119f3cc2ade074736
black996 <rharasis@yahoo.com>
2022-10-17 00:11:30 +0300

    add needed interfaces, utilize them in teams file fns

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,16 +1,21 @@
+import type { ITeam } from '../types';
 import { apiCall } from '../utils/networking';
 
-let cachedAllTeamsList: Record<string,any>;
-export async function getAllTeams() {
+// function isITeam(arg:any): arg is ITeam{
+
+// }
+
+let cachedAllTeamsList: ITeam[];
+export async function getAllTeams() : Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
     cachedAllTeamsList = await apiCall('teams');
 
   return cachedAllTeamsList;
 }
 
-const cachedTeamRecords: Record<string,any> = {};
+const cachedTeamRecords: Record<string,Promise<ITeam>> = {};
 
-export async function getTeamById(id:string) {
+export async function getTeamById(id:string) :Promise<ITeam> {
   let cached = cachedTeamRecords[id];
   if (typeof cached === 'undefined')
     cached = cachedTeamRecords[id] = apiCall(`teams/${id}`);
