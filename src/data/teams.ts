f9233a65eb169e246b780bca055667895b245ac7
black996 <rharasis@yahoo.com>
2022-10-17 23:26:39 +0300

    apply type guard to teams api response, impl generic assertion fn

--- src/data/teams.ts
+++ src/data/teams.ts
@@ -1,14 +1,28 @@
 import type { ITeam } from '../types';
 import { apiCall } from '../utils/networking';
 
-// function isITeam(arg:any): arg is ITeam{
+function isITeam(arg:any): arg is ITeam{
+  return (
+  typeof arg.iconUrl  == "string"
+  && typeof arg.name == "string"
+  && typeof arg.id == "string"
+  && Array.isArray(arg.channels)
+  )
+}
 
-// }
+function assertIsTypedArray<T>(arg: any, typeGuard: (val: any) => val is T): asserts arg is T[] {
+  if (!Array.isArray(arg)) 
+    throw new Error(`arg is not an array ${JSON.stringify(arg)}`);
+
+  if (arg.some((item) => !typeGuard(item))) 
+    throw new Error(`some or all of the items don't follow the expected type. arg: ${JSON.stringify(arg)}`)
+}
 
 let cachedAllTeamsList: ITeam[];
 export async function getAllTeams() : Promise<ITeam[]> {
   if (typeof cachedAllTeamsList === 'undefined')
     cachedAllTeamsList = await apiCall('teams');
+  assertIsTypedArray(cachedAllTeamsList,isITeam);
 
   return cachedAllTeamsList;
 }
